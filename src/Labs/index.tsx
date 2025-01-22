3522220ee423b1fa54094d0bc3e2cbd7c2ba33fa
Stone Holt <stoneholt22@gmail.com>
2025-01-22 18:18:15 -0500

    added name

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,6 +7,8 @@ export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
+      <h2>Stone Holt</h2>
+      <h2>Section: CS5610 - 35649</h2>
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
