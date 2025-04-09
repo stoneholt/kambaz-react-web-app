debb8a4874ec0cdf06a81f384131a10370dfc63c
Stone Holt <stoneholt22@gmail.com>
2025-04-08 22:31:28 -0400

    up to 6.3.1.2

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -5,12 +5,15 @@ export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
   const { pathname } = useLocation();
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
       {
         links.map((link: any) => (
           <div>
-          <Link to={`/Kambaz/Account/${link}`} id="wd-account-signin-link" className={`nav-link ${pathname.includes(link) ? "list-group-item active border border-0" : "list-group-item text-danger border border-0"}`}> {link}  </Link> <br/>
+            <Link to={`/Kambaz/Account/${link}`} id="wd-account-signin-link" className={`nav-link ${pathname.includes(link) ? "list-group-item active border border-0" : "list-group-item text-danger border border-0"}`}> {link}  </Link> <br/>
+            {currentUser && currentUser.role === "ADMIN" && (
+              <Link to={`/Kambaz/Account/Users`} className={`list-group-item ${active("Users")}`}> Users </Link> )}
           </div>
         ))
       }
--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router";
 import Signin from "./Signin";
 import Profile from "./Profile";
 import Signup from "./Signup";
+import Users from "./Users";
 import AccountNavigation from "./Navigation";
 import { useSelector } from "react-redux";
 export default function Account() {
@@ -19,6 +20,8 @@ export default function Account() {
               <Route path="/Signin"  element={<Signin />} />
               <Route path="/Profile" element={<Profile />} />
               <Route path="/Signup"  element={<Signup />} />
+              <Route path="/Users" element={<Users />} />
+              <Route path="/Users/:uid" element={<Users />} />
             </Routes>
           </td>
         </tr>
